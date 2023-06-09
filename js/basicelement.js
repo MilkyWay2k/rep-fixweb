7feddcddd0264fffd86eb2dd7c4004839a43b526
MilkyWay2k <shevelev-2004@list.ru>
2023-06-09 22:26:17 +0300

    1

--- js/basicelement.js
+++ js/basicelement.js
@@ -76,121 +76,145 @@ window.onload = function() {
 
 /////////////////////////
 
-var snakeGame = (function () {
-  var BOARD_SIZE = 16;
-  var CELL_SIZE = 20;
-
-  var board;
+var snakeGame = (function() {
+  var gameBoard;
   var snake;
   var fruit;
   var direction;
   var gameLoopInterval;
+  var currentSpeed;
+  var score;
+  var scoreElement;
 
   function init() {
-      board = document.querySelector('.game-board');
-      board.style.width = (BOARD_SIZE * CELL_SIZE) + 'px';
-      board.style.height = (BOARD_SIZE * CELL_SIZE) + 'px';
-
-      snake = [];
-      var head = createSnakePart(8, 8);
-      snake.push(head);
-
-      fruit = createFruit();
-
+      gameBoard = document.querySelector('.game-board');
+      snake = [{x: 10, y: 10}];
+      fruit = {x: 5, y: 5};
       direction = 'right';
+      currentSpeed = 200;
+      score = 0;
 
-      gameLoopInterval = setInterval(gameLoop, 200);
-
-      document.getElementById('startButton').style.display = 'none';
+      scoreElement = document.createElement('div');
+      scoreElement.className = 'score';
+      scoreElement.textContent = 'Score: ' + score;
+      document.body.appendChild(scoreElement);
   }
 
-  function createSnakePart(x, y) {
-      var part = document.createElement('div');
-      part.className = 'game-snake';
-      part.style.width = (CELL_SIZE - 4) + 'px';
-      part.style.height = (CELL_SIZE - 4) + 'px';
-      part.style.left = (x * CELL_SIZE) + 'px';
-      part.style.top = (y * CELL_SIZE) + 'px';
-      board.appendChild(part);
-      return part;
+  function createSnake() {
+      for (var i = 0; i < snake.length; i++) {
+          var snakeElement = document.createElement('div');
+          snakeElement.className = 'game-snake';
+          snakeElement.style.left = snake[i].x * 10 + 'px';
+          snakeElement.style.top = snake[i].y * 10 + 'px';
+          gameBoard.appendChild(snakeElement);
+      }
   }
 
   function createFruit() {
-      var x = Math.floor(Math.random() * BOARD_SIZE);
-      var y = Math.floor(Math.random() * BOARD_SIZE);
-      var fruit = document.createElement('div');
-      fruit.className = 'game-fruit';
-      fruit.style.width = CELL_SIZE + 'px';
-      fruit.style.height = CELL_SIZE + 'px';
-      fruit.style.left = (x * CELL_SIZE) + 'px';
-      fruit.style.top = (y * CELL_SIZE) + 'px';
-      board.appendChild(fruit);
-      return fruit;
+      var fruitElement = document.createElement('div');
+      fruitElement.className = 'game-fruit';
+      fruitElement.style.left = fruit.x * 10 + 'px';
+      fruitElement.style.top = fruit.y * 10 + 'px';
+      gameBoard.appendChild(fruitElement);
+
+      score++;
   }
 
-  function gameLoop() {
-      var head = snake[0];
-      var x = parseInt(head.style.left) / CELL_SIZE;
-      var y = parseInt(head.style.top) / CELL_SIZE;
+  function moveSnake() {
+      var head = {x: snake[0].x, y: snake[0].y};
+
       switch (direction) {
           case 'up':
-              y--;
-              break;
-          case 'down':
-              y++;
+              head.y--;
               break;
           case 'left':
-              x--;
+              head.x--;
               break;
           case 'right':
-              x++;
+              head.x++;
+              break;
+          case 'down':
+              head.y++;
               break;
       }
 
-      if (x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE) {
-          gameOver();
-          return;
+      snake.unshift(head);
+      snake.pop();
+  }
+
+  function checkCollision() {
+      var head = snake[0];
+
+      // Проверка на столкновение с границами поля
+      if (head.x < 0 || head.x >= 32 || head.y < 0 || head.y >= 32) {
+          return true;
       }
 
-      if (x === parseInt(fruit.style.left) / CELL_SIZE && y === parseInt(fruit.style.top) / CELL_SIZE) {
-          var tail = createSnakePart(parseInt(snake[snake.length - 1].style.left) / CELL_SIZE, parseInt(snake[snake.length - 1].style.top) / CELL_SIZE);
-          snake.push(tail);
-          board.removeChild(fruit);
-          fruit = createFruit();
-      }
-
+      // Проверка на столкновение со своим телом
       for (var i = 1; i < snake.length; i++) {
-          if (x === parseInt(snake[i].style.left) / CELL_SIZE && y === parseInt(snake[i].style.top) / CELL_SIZE) {
-              gameOver();
-              return;
+          if (head.x === snake[i].x && head.y === snake[i].y) {
+              return true;
           }
       }
 
-      var tail = snake.pop();
-      tail.style.left = (x * CELL_SIZE) + 'px';
-      tail.style.top = (y * CELL_SIZE) + 'px';
-      snake.unshift(tail);
+      // Проверка на столкновение с фруктом
+      if (head.x === fruit.x && head.y === fruit.y) {
+          snake.push({});
+          createFruit();
+      }
+
+      return false;
+  }
+
+  function updateGameBoard() {
+      while (gameBoard.firstChild) {
+          gameBoard.firstChild.remove();
+      }
+
+      createSnake();
+      createFruit();
+
+      if (checkCollision()) {
+          gameOver();
+      }
   }
 
   function gameOver() {
       clearInterval(gameLoopInterval);
-      var gameOverContainer = document.querySelector('.game-over');
-      gameOverContainer.style.display = 'flex';
-      setTimeout(function() {
-          gameOverContainer.style.display = 'none';
-          window.location.href = 'https://itmo-history.vercel.app/index.html';
-      }, 5000);
+      document.querySelector('.game-over-text').textContent = 'Вы проиграли! Очки: ' + score;
+      document.querySelector('.game-over').style.display = 'flex';
   }
 
   function changeDirection(newDirection) {
+      if (
+          (direction === 'up' && newDirection === 'down') ||
+          (direction === 'down' && newDirection === 'up') ||
+          (direction === 'left' && newDirection === 'right') ||
+          (direction === 'right' && newDirection === 'left')
+      ) {
+          return;
+      }
+
       direction = newDirection;
   }
 
+  function start() {
+      init();
+      updateGameBoard();
+
+      gameLoopInterval = setInterval(function() {
+          moveSnake();
+          updateGameBoard();
+      }, currentSpeed);
+  }
+
   return {
-      init: init,
-      changeDirection: changeDirection
+      changeDirection: changeDirection,
+      start: start
   };
 })();
+
+snakeGame.start();
 
 function textStyle1() {
   alert("Поздравляем, Вы нашли нашу пасхалку✨ Желаем Вам хорошего настроения и успехов в учебе🎉");
