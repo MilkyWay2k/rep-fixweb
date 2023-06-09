1ec822741c8a6412bdd7aafe0086253a0347ec88
MilkyWay2k <shevelev-2004@list.ru>
2023-06-09 21:55:19 +0300

    1

--- js/basicelement.js
+++ js/basicelement.js
@@ -98,8 +98,6 @@ var snakeGame = (function () {
       fruit = createFruit();
 
       direction = 'right';
-
-      gameLoopInterval = setInterval(gameLoop, 200);
   }
 
   function createSnakePart(x, y) {
@@ -151,44 +149,56 @@ var snakeGame = (function () {
       }
 
       if (x === parseInt(fruit.style.left) / CELL_SIZE && y === parseInt(fruit.style.top) / CELL_SIZE) {
-          var tail = createSnakePart(parseInt(snake[snake.length - 1].style.left) / CELL_SIZE, parseInt(snake[snake.length - 1].style.top) / CELL_SIZE);
-          snake.push(tail);
+          var tail = createSnakePart(x, y);
+          snake.unshift(tail);
           board.removeChild(fruit);
           fruit = createFruit();
+      } else {
+          var tail = snake.pop();
+          tail.style.left = (x * CELL_SIZE) + 'px';
+          tail.style.top = (y * CELL_SIZE) + 'px';
+          snake.unshift(tail);
       }
 
       for (var i = 1; i < snake.length; i++) {
-          if (x === parseInt(snake[i].style.left) / CELL_SIZE && y === parseInt(snake[i].style.top) / CELL_SIZE) {
+          var part = snake[i];
+          if (x === parseInt(part.style.left) / CELL_SIZE && y === parseInt(part.style.top) / CELL_SIZE) {
               gameOver();
               return;
           }
       }
+  }
 
-      var tail = snake.pop();
-      tail.style.left = (x * CELL_SIZE) + 'px';
-      tail.style.top = (y * CELL_SIZE) + 'px';
-      snake.unshift(tail);
+  function changeDirection(newDirection) {
+      if (
+          (newDirection === 'up' && direction !== 'down') ||
+          (newDirection === 'down' && direction !== 'up') ||
+          (newDirection === 'left' && direction !== 'right') ||
+          (newDirection === 'right' && direction !== 'left')
+      ) {
+          direction = newDirection;
+      }
+  }
+
+  function startGame() {
+      document.querySelector('.controls').style.display = 'none';
+      init();
+      gameLoopInterval = setInterval(gameLoop, 200);
   }
 
   function gameOver() {
       clearInterval(gameLoopInterval);
-      var gameOverContainer = document.querySelector('.game-over');
-      gameOverContainer.style.display = 'flex';
-      setTimeout(function() {
-          gameOverContainer.style.display = 'none';
-          window.location.href = 'https://itmo-history.vercel.app/index.html';
-      }, 5000);
-  }
-
-  function changeDirection(newDirection) {
-      direction = newDirection;
+      document.querySelector('.game-over').style.display = 'flex';
   }
 
   return {
-      init: init,
       changeDirection: changeDirection
   };
 })();
+
+function startGame() {
+  snakeGame.startGame();
+}
 
 function textStyle1() {
   alert("Поздравляем, Вы нашли нашу пасхалку✨ Желаем Вам хорошего настроения и успехов в учебе🎉");
